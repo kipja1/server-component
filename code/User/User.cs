14fe51889f9d082cc7143760fea6126eb8e1ea69
kipja1 <84846149+kipja1@users.noreply.github.com>
2021-05-27 21:34:30 +0300

    Add files via upload

---
+++ code/User/User.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+using NLog;
+
+namespace User
+{
+    /// <summary>
+    /// USer client class
+    /// </summary>
+    class User
+    {
+
+        Logger log = LogManager.GetCurrentClassLogger();
+        bool isBattery = false;
+        private static int[] sizes = { 200, 500, 800, 1000 };
+        List<int> possiblBatterySize = new List<int>(sizes);
+        int BatterySize = 0;
+        int EnergyInBattery = 0;
+
+        /// <summary>
+        /// Console log formate
+        /// </summary>
+        private void ConfigureLogging()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var console =
+                new NLog.Targets.ConsoleTarget("console")
+                {
+                    Layout = @"${date:format=HH\:mm\:ss}|${level}| ${message} ${exception}"
+                };
+            config.AddTarget(console);
+            config.AddRuleForAllLevels(console);
+
+            LogManager.Configuration = config;
+        }
+
+        /// <summary>
+        /// Class main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            User self = new User();
+            self.run();
+        }
+
+        /// <summary>
+        /// Class execution method
+        /// </summary>
+        private void run()
+        {
+            ConfigureLogging();
+            var service = new UserService(@"http://localhost:5000/service/");
+
+            //variable for calculation
+            Random rand = new Random();
+            try
+            {
+
+                int userID = service.getUserID();
+                log.Info($"User ID {userID}");
+
+                generateBattery(rand);
+
+                while (true)
+                {
+                    if (isBattery)
+                    {
+                        checkForShortage(service, userID);
+                    }
+                    generate(service, rand, userID);
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                //log whatever exception to console
+                log.Warn(e, "Unhandled exception caught. Will restart main loop.");
+
+                //prevent console spamming
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// <summary>
+        /// Generating users usage of electricity
+        /// </summary>
+        /// <param name="service">Web service object</param>
+        /// <param name="rand">random generator object</param>
+        private void generate(UserService service, Random rand, int userID)
+        {
+            int randomNum = rand.Next(100, 300);
+            if (isBattery)
+            {
+                if (EnergyInBattery < BatterySize)
+                {
+                    double coeficient = rand.NextDouble() * (0.99 - 0.8) + 0.8;
+                    int generated = (int)(BatterySize * 0.1 * coeficient);
+                    EnergyInBattery += generated;
+                    if (EnergyInBattery > BatterySize)
+                    {
+                        EnergyInBattery = BatterySize;
+                    }
+                    log.Info($"User used {randomNum}. There is {EnergyInBattery} of energy stored in the battery.");
+                }
+                else
+                {
+                    log.Info($"User used {randomNum}. There is {EnergyInBattery} of energy stored in the battery.");
+                }
+            }
+            else
+            {
+                log.Info($"User used {randomNum}");
+            }
+            service.changeElectricityUsage(randomNum,userID);
+            //sleep for a while
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Generating information about battery
+        /// </summary>
+        /// <param name="rand">random generator object</param>
+        private void generateBattery(Random rand)
+        {
+            isBattery = rand.Next(100) <= 80 ? true : false;
+            if (isBattery)
+            {
+                int index = rand.Next(possiblBatterySize.Count);
+                BatterySize = possiblBatterySize[index];
+                log.Info($"User has battery of the size {BatterySize}");
+            }
+            else
+            {
+                log.Info($"User does not have battery");
+            }
+            
+        }
+
+        /// <summary>
+        /// Checks is there a shortage in electrical grid
+        /// </summary>
+        /// <param name="service">Web service object</param>
+        /// <param name="ID">user ID</param>
+        private void checkForShortage(UserService service, int ID)
+        {
+            bool isShortage = service.getShortage();
+            if (isShortage)
+            {
+                service.sendElectricity(EnergyInBattery, ID);
+                EnergyInBattery = 0;
+            }
+        }
+
+    }
+}
---
+++ code/User/UserService.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Runtime.Serialization;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+using System.Text;
+
+using NLog;
+using Newtonsoft.Json;
+
+namespace User
+{
+    /// <summary>
+    /// RPC style service wrapper for user.
+    /// </summary>
+    public class UserService
+    {
+        /// <summary>
+        /// Logger for this class.
+        /// </summary>
+        private Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Access lock.
+        /// </summary>
+        private readonly Object accessLock = new object();
+
+        /// <summary>
+        /// HTTP client.
+        /// </summary>
+        private HttpClient httpClient;
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="serviceBaseUri">Base URI of the service.</param>
+        public UserService(String serviceBaseUri)
+        {
+            //validate inputs
+            if (serviceBaseUri == null) throw new ArgumentException("Argument 'serviceBaseUri' is null.");
+
+            //store inputs
+            lock (accessLock)
+            {
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(serviceBaseUri);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+        }
+
+        /// <summary>
+        /// Check if given HTTP response is '200 OK', throw ApplicationException if not.
+        /// </summary>
+        /// <param name="resourceUri">URI of the resource requested.</param>
+        /// <param name="response">Response to check.</param>
+        private void ValidateHttpResponse(String resourceUri, HttpResponseMessage response)
+        {
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var msg =
+                    $"Non '200 OK' response received when requesting '{httpClient.BaseAddress}/{resourceUri}'." +
+                    $"Respone code is '{response.StatusCode}'. Response body is '{response.Content.ReadAsStringAsync().Result}'";
+                throw new ApplicationException(msg);
+            }
+        }
+
+        /// <summary>
+        /// Changes the number of electricity usage
+        /// </summary>
+        /// <param name="usage">how much electricity this user uses</param>
+        /// <param name="ID">users ID</param>
+        public void changeElectricityUsage(int usage, int ID)
+        {
+            lock (accessLock)
+            {
+                //compose resource URI part with relevan query parameters
+                var resourceUri =
+                    $"changeElectricityUsage?" +
+                    $"usage={HttpUtility.UrlEncode("" + usage)}&" +
+                    $"ID={HttpUtility.UrlEncode("" + ID)}";
+
+                //send HTTP request to service server, get response back
+                var httpResp = httpClient.GetAsync(resourceUri).Result;
+
+                //check if HTTP response is '200 OK', indicate error otherwise
+                ValidateHttpResponse(resourceUri, httpResp);
+                                
+            }
+        }
+
+        /// <summary>
+        /// Gets unique user id
+        /// </summary>
+        /// <returns>User id</returns>
+        public int getUserID()
+        {
+            lock (accessLock)
+            {
+                //compose resource URI part with relevan query parameters
+                var resourceUri =
+                    $"getUserID";
+                //send HTTP request to service server, get response back
+                var httpResp = httpClient.GetAsync(resourceUri).Result;
+
+                //check if HTTP response is '200 OK', indicate error otherwise
+                ValidateHttpResponse(resourceUri, httpResp);
+
+                //extract operation result from string returned
+                var result = Int32.Parse(httpResp.Content.ReadAsStringAsync().Result);
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets bool is there a shortage in the electrical grid
+        /// </summary>
+        /// <returns>User id</returns>
+        public bool getShortage()
+        {
+            lock (accessLock)
+            {
+                //compose resource URI part with relevan query parameters
+                var resourceUri =
+                    $"getShortage";
+
+                //send HTTP request to service server, get response back
+                var httpResp = httpClient.GetAsync(resourceUri).Result;
+
+                //check if HTTP response is '200 OK', indicate error otherwise
+                ValidateHttpResponse(resourceUri, httpResp);
+
+                //extract operation result from string returned
+                var result = Boolean.Parse(httpResp.Content.ReadAsStringAsync().Result);
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Sends electricity in case of the shortage from a battery.
+        /// </summary>
+        /// <param name="energy">how much electricity this user sent</param>
+        /// <param name="ID">users ID</param>
+        public void sendElectricity(int energy, int ID)
+        {
+            lock (accessLock)
+            {
+                //compose resource URI part with relevan query parameters
+                var resourceUri =
+                    $"sendElectricity?" +
+                    $"energy={HttpUtility.UrlEncode("" + energy)}&" +
+                    $"ID={HttpUtility.UrlEncode("" + ID)}";
+
+                //send HTTP request to service server, get response back
+                var httpResp = httpClient.GetAsync(resourceUri).Result;
+
+                //check if HTTP response is '200 OK', indicate error otherwise
+                ValidateHttpResponse(resourceUri, httpResp);
+            }
+        }
+
+    }
+}
